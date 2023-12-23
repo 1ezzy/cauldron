2cfbf7f523e61b00f68777f0f25b13750d20c848
1ezzy <ezra.furry@gmail.com>
2023-12-23 02:43:00 -0500

    undo prisma stuff

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,9 +1,5 @@
-import { PrismaClient as PrismaClientNode } from '@prisma/client';
 import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
-import { ACCELERATE_URL, MONGODB_URL } from '$env/static/private';
-import { dev } from '$app/environment';
+import { ACCELERATE_URL } from '$env/static/private';
 
-const prisma = dev
-	? new PrismaClientNode({ datasources: { db: { url: MONGODB_URL } } })
-	: new PrismaClientEdge({ datasources: { db: { url: ACCELERATE_URL } } });
+const prisma = new PrismaClientEdge({ datasources: { db: { url: ACCELERATE_URL } } });
 export { prisma };
