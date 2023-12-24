cec19e7345c47e7fe8ab1fb6bf7a9178d4342c47
1ezzy <ezra.furry@gmail.com>
2023-12-23 22:43:07 -0500

    attempt to force edge client

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -2,11 +2,8 @@ import { PrismaClient as PrismaClientNode } from '@prisma/client';
 import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { ACCELERATE_URL, MONGODB_URL } from '$env/static/private';
-import { dev } from '$app/environment';
 
-const prisma = dev
-	? new PrismaClientNode({ datasources: { db: { url: MONGODB_URL } } })
-	: new PrismaClientEdge({ datasources: { db: { url: ACCELERATE_URL } } }).$extends(
-			withAccelerate()
-	  );
+const prisma = new PrismaClientEdge({ datasources: { db: { url: ACCELERATE_URL } } }).$extends(
+	withAccelerate()
+);
 export { prisma };
