726155e92984d5fc7aaead2a7e2e122a77ade7f4
1ezzy <ezra.furry@gmail.com>
2023-12-28 00:21:42 -0500

    basic spellbook creation form validation

--- src/routes/spellbooks/create/+page.server.ts
+++ src/routes/spellbooks/create/+page.server.ts
@@ -9,12 +9,16 @@ import { superValidate } from 'sveltekit-superforms/server';
 const userStore = writable('');
 
 const createSpellbookSchema = z.object({
-	name: z.string().max(50),
-	characterName: z.string().max(50),
-	description: z.string().max(500),
-	class1: z.enum(CastingClassType),
-	class2: z.optional(z.enum(CastingClassType)),
-	class3: z.optional(z.enum(CastingClassType))
+	name: z
+		.string({ required_error: 'Name is required' })
+		.min(1)
+		.max(50, { message: 'Must be less than 50 characters' }),
+	characterName: z
+		.string({ required_error: 'Character name is required' })
+		.min(1)
+		.max(50, { message: 'Must be less than 50 characters' }),
+	description: z.string().max(500, { message: 'Must be less than 500 characters' }),
+	classes: z.array(z.enum(CastingClassType)).nonempty({ message: 'Select at least one class' })
 });
 
 export const load = (async ({ locals }) => {
@@ -36,10 +40,8 @@ export const actions = {
 			return fail(400, { form });
 		}
 
-		const classes = [form.data.class1, form.data.class2, form.data.class3];
-
 		const classesMapped = JSON.stringify(
-			classes
+			form.data.classes
 				.filter((className) => className !== undefined)
 				.map((className) => {
 					return { index: className };
