956d793c9ddfd3098468c86f0d09bc363475d3ae
1ezzy <ezra.furry@gmail.com>
2024-02-16 23:16:50 -0500

    think thisll fix it!

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,5 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
-import { v2 as cloudinary } from 'cloudinary';
-import {
-	CLOUDINARY_CLOUD_NAME,
-	CLOUDINARY_API_KEY,
-	CLOUDINARY_API_SECRET
-} from '$env/static/private';
-
-cloudinary.config({
-	cloud_name: CLOUDINARY_CLOUD_NAME,
-	api_key: CLOUDINARY_API_KEY,
-	api_secret: CLOUDINARY_API_SECRET
-});
 
 export const load = (async ({ locals }) => {
 	const session = await locals.auth.validate();
